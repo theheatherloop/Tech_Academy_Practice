0352ea3401e68e6bc3f24b144c9344162eeb782d
Heather Shultz <silverlycold@gmail.com>
2022-09-05 19:24:41 -0500

    organized practice and project folders

--- HTML-and-CSS-Projects/templates_project/Hello_World.js
+++
@@ -1,8 +0,0 @@
-var greeting = 'Hello, World!';
-console.log(greeting);
-
-var greeting2 = 'Welcome to Node.hs';
-console.log(greeting2);
-
-var sentence1 = 'Learning how to debug cude with the debugger';
-console.log(sentence1)
